4b0c74b1c506df1e719bfc27c68c316385695b48
Frebbers <frederik.bode@hendrichsen.dk>
2023-10-27 15:51:10 +0200

    created language class, fields, methods

--- Language.java
+++ Language.java
@@ -1,3 +1,15 @@
 public class Language {
-    
+    private String locale;
+    String[] phrase;
+    String[] errorPhrase;
+
+    public Language (String locale, String[] phrase, String[] errorPhrase){
+        this.locale = locale;
+        this.phrase = phrase;
+        this.errorPhrase = errorPhrase;
+    }
+    public String getLocale() {return this.locale;}
+    public String phraseOut(int input) {return phrase[input];}
+    public String errorPhraseOut(int input) {return errorPhrase[input];}
+
 }
