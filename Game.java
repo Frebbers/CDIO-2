7d2c72aac0c042c82cc6a8b09f4c16b3ee008011
Frebbers <frederik.bode@hendrichsen.dk>
2023-10-27 21:08:36 +0200

    skabt færdiggjort main metoden

--- Game.java
+++ Game.java
@@ -2,6 +2,20 @@ public class Game {
 
 
     public static void main(String[] args){
+    //builds the game
+    Game game = new Game("Dimly lit places prisons and firebreathing birds", 
+    new Player(1,1000), new Player(2,1000), 
+    new Die(6), new Die (6), 
+    new Printer(Language.createDanishLanguage()));
+    
+    while (!game.checkWinner(game.player1) && !game.checkWinner(game.player2)){
+        
+        game.playTurn(game.player1);
+        if (!game.checkWinner(game.player1)) {game.playTurn(game.player2);}
+        
+
+    }
+    
     }
 
 
@@ -17,16 +31,17 @@ public class Game {
     
 
 //game constructor
-    public Game(String name, Player player1, Player player2, Die die1, Die die2, Printer printer, int[] scoreCard) {
+    public Game(String name, Player player1, Player player2, Die die1, Die die2, Printer printer) {
         this.name = name;
         this.player1 = player1;
         this.player2 = player2;
         this.die1 = die1;
         this.die2 = die2;
         this.printer = printer;
+        scoreCard = new int[11];
 
        //assign values to scorecard
-        scoreCard[0]=250;
+        scoreCard[0]=  250;
         scoreCard[1]=-100;
         scoreCard[2]= 100;
         scoreCard[3]= -20;
@@ -58,7 +73,8 @@ public class Game {
     //print error 0, invalid dice roll
     else {this.printer.printErrorPhrase(0);}
     //print end game message if winner has been found.
-    if(checkWinner(player)){Printer.printWinnerPhrase(player);}
+    if(checkWinner(player)){ 
+        printer.printWinnerPhrase(player.getId());}
     //if no one has won and player is on field 10, play another turn.
     else if (field==10){playTurn(player);}
 
