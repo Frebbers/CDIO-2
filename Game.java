22e079804ca50adb41e5d97134d25742d29466cf
Frebbers <frederik.bode@hendrichsen.dk>
2023-10-27 16:56:22 +0200

    Created all methods except playturn Created fields and constructor

---
+++ Game.java
@@ -0,0 +1,28 @@
+public class Game {
+    private String name;
+    private Die die1;
+    private Die die2;
+    private Player player1;
+    private Player player2;
+    private Printer printer;
+
+    public Game(String name, Player player1, Player player2, Die die1, Die die2, Printer printer) {
+        this.name = name;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.die1 = die1;
+        this.die2 = die2;
+        this.printer = printer;
+    }
+
+    //check me later!
+    public boolean checkWinner(return ((player1.getScore() => 3000) || (player2.getScore() => 3000)))
+    public void playTurn(Player currentPlayer){
+
+    }
+//Enables changing language
+    public void setPrinter(Printer newPrinter){this.printer = newPrinter;}
+
+
+
+}
