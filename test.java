8c6cf9a5e731db8c8e316e034d65d2a5799ff722
Frebbers <frederik.bode@hendrichsen.dk>
2023-10-28 15:07:14 +0200

    Changed comments on test class

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
 
-
+    //method for testing game
     public static void testGame(){
     Game testGame = new Game("Test game", 
     new Player(1,1000), 
@@ -8,14 +8,23 @@ public class test {
     new Die(6), new Die (6), 
     new Printer(Language.createDanishLanguage()));
 
+    //checks if anyone has won.
     while (!testGame.checkWinner(testGame.player1) && !testGame.checkWinner(testGame.player2)) {
+    //checks if player1 should have won and prints an error if they should
         if (testGame.player1.getScore() >3000) { System.out.println("ERROR! PLAYER 1 HAS ALREADY WON");}
+        System.out.println("Begin turn player1");
+        //plays a turn with player1
         testGame.playTurn(testGame.player1);
-        if (!testGame.checkWinner(testGame.player2)) {
+        //checks if player1 has won, then if player2's score is above 3000. Prints an error if they have already won.
+        if (!testGame.checkWinner(testGame.player1)) {
             if (testGame.player2.getScore() > 3000) { System.out.println("ERROR! PLAYER 2 HAS ALREADY WON");}
+             System.out.println("Begin turn player2");
+             //plays a turn with player 2 as the player
             testGame.playTurn(testGame.player2);}
     }
+    System.out.println("Game has ended");
 }
+//Method for testing dice
 //Creates 1000 dice and rolls each of them, checking if any are invalid
 public static void testDie(){
     Die[] thousandDice = new Die[1000];
