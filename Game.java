064e50e28aade1d56917d7055ee0e20d17b0aabb
Frebbers <frederik.bode@hendrichsen.dk>
2023-10-28 15:46:11 +0200

    enabled testGame() in main method

--- Game.java
+++ Game.java
@@ -5,7 +5,7 @@ public class Game {
 
     public static void main(String[] args){
         test.testDie();
-       // test.testGame();
+       test.testGame();
     }
 
     public static void playGame(){
